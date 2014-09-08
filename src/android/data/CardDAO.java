ea86186f803837a97327dd5406fe4b6307d1f7a4
Juan Jara <juanjojaray@gmail.com>
2014-09-08 11:36:35 +0200

    no message

--- src/android/data/CardDAO.java
+++ src/android/data/CardDAO.java
@@ -2,6 +2,7 @@ package com.tenforwardconsulting.cordova.bgloc.data;
 
 public interface CardDAO {
 	public void internetPendingCards();
+	public int getCardId();
 	public boolean persistCard(String table, Card card);
 	//public void deleteCard(Card l);
 }
