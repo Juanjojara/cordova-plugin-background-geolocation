61ac784749cfa9adf3861124fc19790e65164eef
Juan Jara <juanjojaray@gmail.com>
2014-10-13 17:14:29 +0200

    Updates to the Plugin

--- www/BackgroundGeoLocation.js
+++ www/BackgroundGeoLocation.js
@@ -1,8 +1,18 @@
 var exec = require("cordova/exec");
 module.exports = {
+    /**
+    * @property {Object} stationaryRegion
+    */
+    stationaryRegion: null,
+    /**
+    * @property {Object} config
+    */
+    config: {},
+
     configure: function(success, failure, config) {
+        this.config = config;
         var params              = JSON.stringify(config.params || {}),
-	    headers		= JSON.stringify(config.headers || {}),
+	        headers		        = JSON.stringify(config.headers || {}),
             url                 = config.url        || 'BackgroundGeoLocation_url',
             stationaryRadius    = (config.stationaryRadius >= 0) ? config.stationaryRadius : 50,    // meters
             distanceFilter      = (config.distanceFilter >= 0) ? config.distanceFilter : 500,       // meters
@@ -10,13 +20,16 @@ module.exports = {
             desiredAccuracy     = (config.desiredAccuracy >= 0) ? config.desiredAccuracy : 100,     // meters
             debug               = config.debug || false,
             notificationTitle   = config.notificationTitle || "Background tracking",
-            notificationText    = config.notificationText || "ENABLED";
+            notificationText    = config.notificationText || "ENABLED",
+            activityType        = config.activityType || "OTHER",
+            stopOnTerminate     = config.stopOnTerminate || false;
 
         exec(success || function() {},
              failure || function() {},
              'BackgroundGeoLocation',
              'configure',
-             [params, headers, url, stationaryRadius, distanceFilter, locationTimeout, desiredAccuracy, debug, notificationTitle, notificationText]);
+             [params, headers, url, stationaryRadius, distanceFilter, locationTimeout, desiredAccuracy, debug, notificationTitle, notificationText, activityType, stopOnTerminate]
+        );
     },
     start: function(success, failure, config) {
         exec(success || function() {},
@@ -53,6 +66,7 @@ module.exports = {
     * @param {Integer} timeout
     */
     setConfig: function(success, failure, config) {
+        this.apply(this.config, config);
         exec(success || function() {},
             failure || function() {},
             'BackgroundGeoLocation',
@@ -68,5 +82,32 @@ module.exports = {
             'BackgroundGeoLocation',
             'getStationaryLocation',
             []);
-       }
+    },
+    /**
+    * Add a stationary-region listener.  Whenever the devices enters "stationary-mode", your #success callback will be executed with #location param containing #radius of region
+    * @param {Function} success
+    * @param {Function} failure [optional] NOT IMPLEMENTED
+    */
+    onStationary: function(success, failure) {
+        var me = this;
+        success = success || function() {};
+        var callback = function(region) {
+            me.stationaryRegion = region;
+            success.apply(me, arguments);
+        };
+        exec(callback,
+            failure || function() {},
+            'BackgroundGeoLocation',
+            'addStationaryRegionListener',
+            []);
+    },
+    apply: function(destination, source) {
+        source = source || {};
+        for (var property in source) {
+            if (source.hasOwnProperty(property)) {
+                destination[property] = source[property];
+            }
+        }
+        return destination;
+    }
 };
